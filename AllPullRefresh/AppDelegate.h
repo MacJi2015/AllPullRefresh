81c7e81be568b2711f04d808f06b6cbcaa1356fe
jibai <1439125361@qq.com>
2018-07-05 12:12:19 +0800

    Initial Commit

---
+++ AllPullRefresh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AllPullRefresh
+//
+//  Created by 郭嘉 on 2018/7/5.
+//  Copyright © 2018年 郭嘉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
